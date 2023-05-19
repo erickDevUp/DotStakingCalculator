7b80dce162a21de93378556865d85ae9a8072624
erickdevup <erickdevup@gmail.com>
2023-05-19 08:24:09 +0200

    add analitics google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,25 @@
 import SeoTags from "@/components/utils/seoTags";
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <SeoTags />
+      <Head>
+        <SeoTags />
+      </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-C4JQG2K24N"
+      ></Script>
+      <Script>
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C4JQG2K24N');`}
+      </Script>
       <body>
         <Main />
         <NextScript />
