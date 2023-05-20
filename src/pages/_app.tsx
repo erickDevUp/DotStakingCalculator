4acef56413be16c9bdd78da5d84d44a04aa9f99f
erickdevup <erickdevup@gmail.com>
2023-05-21 00:45:56 +0200

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import SeoTags from "@/components/utils/seoTags";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <SeoTags />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-C4JQG2K24N"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-C4JQG2K24N');`}
+        </Script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,22 +5,7 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <SeoTags />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-C4JQG2K24N"
-          strategy="afterInteractive"
-        ></Script>
-        <Script>
-          {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-C4JQG2K24N');`}
-        </Script>
-      </Head>
-
+      <Head />
       <body>
         <Main />
         <NextScript />
