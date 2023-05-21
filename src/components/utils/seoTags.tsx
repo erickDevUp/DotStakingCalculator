b8e4b472562527658efece37b0ede6bf0b59fcc3
erickdevup <erickdevup@gmail.com>
2023-05-21 10:23:43 +0200

    seo ligtehause

--- src/components/utils/seoTags.tsx
+++ src/components/utils/seoTags.tsx
@@ -59,9 +59,14 @@ export default function SeoTags() {
         content="Polkadot Staking Calculator,polkadot staking rewards  , Polkadot Staking Calculator Reward ,staking dot calculator,dot Staking Calculator  ,dot Staking Calculator Reward "
       />
       <meta
-        name="copyright"
-        content="Polkadot Staking Calculator Reward (DOT)"
+        name="description"
+        content="Get the most out of your Polkadot staking with our efficient and distraction-free calculator. Calculate your rewards hassle-free with our easy-to-use tool."
       />
+      <meta
+        name="copyright"
+        content="Polkadot Staking Calculator Reward (ETH)"
+      />
+      <title>Polkadot Staking Calculator Reward (ETH)</title>
     </>
   );
 }
