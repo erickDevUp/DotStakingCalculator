84b6a2bc53cb076402737fd9e52519c9825f2572
erickdevup <erickdevup@gmail.com>
2023-05-21 08:34:53 +0200

    seo fix

--- src/components/utils/seoTags.tsx
+++ src/components/utils/seoTags.tsx
@@ -1,76 +1,67 @@
+import { NextSeo } from "next-seo";
+
 export default function SeoTags() {
   return (
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta
-        name="description"
-        content="Get the most out of your Polkadot staking with our efficient and distraction-free calculator. Calculate your rewards hassle-free with our easy-to-use tool."
+      <NextSeo
+        title="Polkadot Staking Calculator Reward (DOT)"
+        description="Get the most out of your Polkadot staking with our efficient and distraction-free calculator. Calculate your rewards hassle-free with our easy-to-use tool."
+        canonical="https://dot-staking-calculator.vercel.app/"
+        additionalMetaTags={[
+          {
+            property: "dc:creator",
+            content: "erickDevUP",
+            keyOverride: "creator1",
+          },
+        ]}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "https://dot-staking-calculator.vercel.app/favicon.ico",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "https://dot-staking-calculator.vercel.app/favicon.ico",
+            sizes: "76x76",
+          },
+          {
+            rel: "manifest",
+            href: "/manifest.json",
+          },
+        ]}
+        openGraph={{
+          url: "https://www.url.ie/a",
+          title: "Polkadot Staking Calculator Reward (DOT)",
+          description:
+            "Get the most out of your Polkadot staking with our efficient and distraction-free calculator. Calculate your rewards hassle-free with our easy-to-use tool.",
+          images: [
+            {
+              url: "https://dot-staking-calculator.vercel.app/shot.png",
+              width: 900,
+              height: 800,
+              alt: "Polkadot Staking Calculator Reward (DOT)",
+              type: "image/png",
+            },
+          ],
+          siteName: "Polkadot Staking Calculator Reward (DOT)",
+        }}
+        twitter={{
+          handle: "@erickdevup",
+          site: "https://dot-staking-calculator.vercel.app/",
+          cardType: "summary_large_image",
+        }}
       />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       <meta
         name="keywords"
-        content="Polkadot Staking Calculator  , Polkadot Staking Calculator Reward ,dot Staking Calculator  ,dot Staking Calculator Reward "
+        content="Polkadot Staking Calculator,polkadot staking rewards  , Polkadot Staking Calculator Reward ,staking dot calculator,dot Staking Calculator  ,dot Staking Calculator Reward "
       />
       <meta
         name="copyright"
         content="Polkadot Staking Calculator Reward (DOT)"
       />
-      <meta name="author" content="erickDevUP" />
-      <meta name="robots" content="index" />
-      <meta name="robots" content="follow" />
-
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:url"
-        content="https://dot-staking-calculator.vercel.app"
-      />
-      <meta
-        property="og:title"
-        content="Polkadot Staking Calculator Reward (DOT)"
-      />
-      <meta
-        property="og:description"
-        content="Get the most out of your Polkadot staking with our efficient and distraction-free calculator. Calculate your rewards hassle-free with our easy-to-use tool."
-      />
-      <meta
-        property="og:image"
-        content="https://dot-staking-calculator.vercel.app/shot.png"
-        
-      />
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta
-        property="twitter:url"
-        content="https://dot-staking-calculator.vercel.app"
-      />
-      <meta
-        property="twitter:site"
-        content="https://dot-staking-calculator.vercel.app"
-      />
-      <meta
-        property="twitter:title"
-        content="Polkadot Staking Calculator Reward (DOT)"
-      />
-      <meta
-        property="twitter:description"
-        content="Get the most out of your Polkadot staking with our efficient and distraction-free calculator. Calculate your rewards hassle-free with our easy-to-use tool."
-      />
-      <meta
-        property="twitter:image"
-        content="https://dot-staking-calculator.vercel.app/shot.png"
-      />
-
-      <link rel="manifest" href="/manifest.json" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="https://dot-staking-calculator.vercel.app/favicon.ico"
-      />
-
-      <link
-        rel="icon"
-        type="image/svg+xml"
-        href="https://dot-staking-calculator.vercel.app/favicon.ico"
-      />
-      <title>Polkadot Staking Calculator Reward (DOT)</title>
     </>
   );
 }
